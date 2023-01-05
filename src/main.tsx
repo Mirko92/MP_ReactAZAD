af3ea6b571d7eebec78f0d1bb639492cea8b0934
Mirko Petrelli <mirko.petrelli@avanade.com>
2023-01-05 16:11:06 +0100

    refactoring solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,60 +1,18 @@
 import { MsalProvider } from '@azure/msal-react';
-import { 
-  PublicClientApplication, 
-  LogLevel 
-} from "@azure/msal-browser";
 
-import React                       from "react";
-import ReactDOM                    from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React              from "react";
+import ReactDOM           from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import { loginRequest, msalConfig } from "./configs/authConfig";
-
-// Pages
-import Home  from "./pages/Home";
+import router       from "./router";
+import msalInstance from './msal';
 
 import "./index.css"; 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
-const msalInstance  = new PublicClientApplication({
-  ...msalConfig,
-
-  system: {
-    loggerOptions: {
-      logLevel: LogLevel.Error
-    }
-  }
-});
-
-// Try to perform SsoSilent request.
-// If it fails, clear Local and Session storage
-msalInstance
-  .ssoSilent({
-    ...loginRequest,
-    state: "Check for an active token"
-  })
-  .catch(e => {
-    console.error("Error on cheking for an active token. \n", e)
-
-    localStorage.clear()
-    sessionStorage.clear()
-    
-  })
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-      {/* <RouterProvider router={router} /> */}
-      <Home />
+      <RouterProvider router={router} />
     </MsalProvider>
   </React.StrictMode>
 );
---
+++ src/msal.tsx
@@ -0,0 +1,30 @@
+import { LogLevel, PublicClientApplication } from "@azure/msal-browser";
+import { loginRequest, msalConfig } from "./configs/authConfig";
+
+const msalInstance  = new PublicClientApplication({
+  ...msalConfig,
+
+  system: {
+    loggerOptions: {
+      logLevel: LogLevel.Error
+    }
+  }
+});
+
+// Try to perform SsoSilent request.
+// If it fails, clear Local and Session storage
+msalInstance
+  .ssoSilent({
+    ...loginRequest,
+    state: "Check for an active token"
+  })
+  .catch(e => {
+    console.error("Error on cheking for an active token. \n", e)
+
+    localStorage.clear()
+    sessionStorage.clear()
+    
+  })
+
+
+export default msalInstance;
