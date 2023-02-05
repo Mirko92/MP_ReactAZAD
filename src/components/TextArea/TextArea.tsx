74018223203834f0a3b374d0e93b5c2f2dcc8e56
Mirko <mirko.petrelli@outlook.it>
2023-02-05 12:25:30 +0100

    Three colored section token string

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -6,11 +6,17 @@ interface TextAreaProps {
 }
 
 export function TextArea({ text, isLoading }: TextAreaProps) {
+
+
   return <div className="text-area hvr-grow-shadow">
     {
       isLoading
         ? 'isLoading'
-        :  text
+        :  text?.split('.')
+                .map( (x,i) => (<>
+                  <span className={`token-part-${i+1}`}>{x}</span>
+                  {i < 2 && <span><b>.</b></span>}
+                </>))
     }
   </div>
 }
